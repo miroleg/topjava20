d2a0575e8df365dce72fe0f7d133dde53894851e
Oleg Mironenko <atek@ngs.ru>
2021-02-19 10:04:12 +0700

    topjava20    19/02/2021

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,5 +1,5 @@
 package ru.javawebinar.topjava.model;
-// topjava20    18/02/2021
+// topjava20    19/02/2021
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import org.hibernate.annotations.OnDelete;
